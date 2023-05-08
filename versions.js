6c694fd563d4e713375d7b588be7fd026bdcee34
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2023-05-08 02:38:45 +0000

    build based on c8249204b

---
+++ versions.js
@@ -0,0 +1,13 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v1.8",
+  "v1.7",
+  "v1.6",
+  "v1.5",
+  "v1.4",
+  "v1.3",
+  "v1.2",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v1.8.2";
+var DOCUMENTER_STABLE = "stable";
